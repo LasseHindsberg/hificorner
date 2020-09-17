c61d2d9fe8df3ea2924a335ef0a223d1739f012f
Andreas <andreasbarkov@gmail.com>
2020-09-17 12:56:28 +0200

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
-document.addEventListener("DOMContentLoaded", e=> {
-//
-});
+var url = new URLSearchParams(window.location.search);
+
+if (!url.get("id")) {
+    showProducts();
+}
+
+
+function showProducts(){
+    getProducts()
+    .then(function(products){
+        let allProducts = document.querySelector(".products");
+
+        products.forEach(function(product) { 
+            const productContainer = document.createElement('div');
+            productContainer.classList.add('product');
+            
+            productContainer.innerHTML = `
+                <img src="${product.image}" alt="${product.name}">
+                <p>${product.name}</p>
+                <p class="price"><span class="oldPrice">${product.oldPrice}</span>${product.price}</p>
+                <a href="" class="productLink">ADD TO CART</a>
+            `
+            productContainer.querySelector(".productLink").href = `/product.html?id=${product.id}`;
+            allProducts.appendChild(productContainer);
+        });
+    });
+}
+
+function getProducts(){
+    return fetch("./data.json")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        return data.product;
+    });
+}
