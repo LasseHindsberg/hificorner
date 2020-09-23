85a9c108ee37a4a48d510a44121e84e21d3faabb
Lasse Hindsberg <3404293@rts-365.dk>
2020-09-23 12:19:37 +0200

    teehee uwu

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,8 +1,19 @@
-let url = new URLSearchParams(window.location.search);
-if (url.get("keyword")) {
-    fetch(`.././data.json${url.get("keyword")}`)
-    .then(res =>res.json())
-    .then(function(data) {
+let searchUrl = new URLSearchParams(window.location.search);
 
-    });
-}
+if (searchUrl.get("keyword")) {
+    fetch(`./data.json`)
+    .then(data => data.json())
+    .then(function(data) {
+        
+        console.log(data);
+        let resultArray = [];
+
+        data.product.forEach(function(product){
+            if (searchUrl.get("keyword") == product.name) {
+                resultArray.push(product);
+            }
+
+        });
+    })
+
+};
