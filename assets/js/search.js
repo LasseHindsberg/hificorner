82721ad0ac871c4b9314e6d6bc45035014792d86
Emil Heldorf Knudsen <6611164@RTS-365.dk>
2020-09-24 11:00:15 +0200

    Søge funktion

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,15 +5,27 @@ if (searchUrl.get("keyword")) {
     .then(data => data.json())
     .then(function(data) {
         
-        console.log(data);
-        let resultArray = [];
+        console.log(data.product);
+        let resultArray = data.product;
 
-        data.product.forEach(function(product){
-            if (searchUrl.get("keyword") == product.name) {
+        //resultArray.name.forEach(function(product){
+            /*if (searchUrl.get("keyword") == product.name) {
                 resultArray.push(product);
+            }*/
+            resultArray.forEach(myFunction);
+
+      
+            function myFunction(item) {
+                let name = item.name.toLowerCase();
+                let description = item.description.toLowerCase();
+                let keyword = searchUrl.get("keyword").toLowerCase();
+                if (name.includes(keyword) ||  description.includes(keyword)) {
+                    document.getElementById("products").innerHTML += item.name + "<br>";
+                    //console.log(item.name);
+                }
             }
 
-        });
-    })
+       // });
+    });
 
 };
